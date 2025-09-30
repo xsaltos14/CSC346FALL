1249fb57f3c43af7e23e793a950d36b8c85aff90
Xavier Saltos <xaiversaltos14@gmail.com>
2025-09-30 11:13:51 -0400

    Initial Commit

---
+++ stackandQ/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  stackandQ
+//
+//  Created by xavier saltos on 9/30/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
